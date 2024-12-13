5279117a1944f9cfb5e64bfbec287767070215a6
Souzatitan <asouzanderson@gmail.com>
2024-12-12 22:29:49 -0300

    commit2

---
+++ assets/scripts/characters.js
@@ -0,0 +1,53 @@
+const characterDescriptions = {
+    Eleven: {
+        title: "Você seria, Eleven em Stranger Things",
+        description: "Parabéns! Você é poderosa, protetora e incrivelmente leal, como Eleven. Com uma força extraordinária e um coração grande, você luta contra o mundo invertido com coragem e determinação.",
+        image: "assets/images/Eleven.webp"
+    },
+    Dustin: {
+        title: "Você seria, Dustin Henderson em Stranger Things",
+        description: "Parabéns! Você é Leal, engraçado e incrivelmente inteligente como o Dustin, que é o coração do grupo de amigos em Stranger Things. Com seu senso de humor contagiante e curiosidade científica, você enfrenta os desafios com criatividade e otimismo.",
+        image: "path/to/dustin-image.jpg"
+    },
+    // Adicione os outros personagens aqui
+    Mike: {
+        title: "Você seria, Mike em Stranger Things",
+        description: "Você é um líder natural, inspirador e determinado a proteger os seus amigos. Sempre pronto para criar planos ousados, sua força está em unir as pessoas ao seu redor.",
+        image: "path/to/mike-image.jpg"
+    },
+    will: {
+        title: "Você seria, Mike em Stranger Things",
+        description: "Você é um líder natural, inspirador e determinado a proteger os seus amigos. Sempre pronto para criar planos ousados, sua força está em unir as pessoas ao seu redor.",
+        image: "path/to/mike-image.jpg"
+    },
+    Mike: {
+        title: "Você seria, Mike em Stranger Things",
+        description: "Você é um líder natural, inspirador e determinado a proteger os seus amigos. Sempre pronto para criar planos ousados, sua força está em unir as pessoas ao seu redor.",
+        image: "path/to/mike-image.jpg"
+    },
+    Mike: {
+        title: "Você seria, Mike em Stranger Things",
+        description: "Você é um líder natural, inspirador e determinado a proteger os seus amigos. Sempre pronto para criar planos ousados, sua força está em unir as pessoas ao seu redor.",
+        image: "path/to/mike-image.jpg"
+    },
+    Mike: {
+        title: "Você seria, Mike em Stranger Things",
+        description: "Você é um líder natural, inspirador e determinado a proteger os seus amigos. Sempre pronto para criar planos ousados, sua força está em unir as pessoas ao seu redor.",
+        image: "path/to/mike-image.jpg"
+    },
+    Mike: {
+        title: "Você seria, Mike em Stranger Things",
+        description: "Você é um líder natural, inspirador e determinado a proteger os seus amigos. Sempre pronto para criar planos ousados, sua força está em unir as pessoas ao seu redor.",
+        image: "path/to/mike-image.jpg"
+    },
+    Mike: {
+        title: "Você seria, Mike em Stranger Things",
+        description: "Você é um líder natural, inspirador e determinado a proteger os seus amigos. Sempre pronto para criar planos ousados, sua força está em unir as pessoas ao seu redor.",
+        image: "path/to/mike-image.jpg"
+    },
+    Mike: {
+        title: "Você seria, Mike em Stranger Things",
+        description: "Você é um líder natural, inspirador e determinado a proteger os seus amigos. Sempre pronto para criar planos ousados, sua força está em unir as pessoas ao seu redor.",
+        image: "path/to/mike-image.jpg"
+    },
+};    
